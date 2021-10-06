f2c3683b2c9e4a323774a8e8d7071fd22aa68766
Kasper <kasperscoop@gmail.com>
2021-10-06 09:39:35 +0200

    small changes

--- Assignments/Assignment1/obj/Debug/net5.0/RazorDeclaration/Pages/Adults.razor.g.cs
+++ Assignments/Assignment1/obj/Debug/net5.0/RazorDeclaration/Pages/Adults.razor.g.cs
@@ -112,7 +112,7 @@ using Assignment1.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 80 "C:\Users\kaspe\OneDrive - ViaUC\Diplomingenioer\3.sem\DNP\Solutions\DNP1\Assignments\Assignment1\Pages\Adults.razor"
+#line 81 "C:\Users\kaspe\OneDrive - ViaUC\Diplomingenioer\3.sem\DNP\Solutions\DNP1\Assignments\Assignment1\Pages\Adults.razor"
            
 
         private IList<Family> familiesToShow;
