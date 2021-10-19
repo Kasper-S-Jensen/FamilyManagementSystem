d2f09d099cdd869746e3661b26a2c78ff01d3377
Kasper <kasperscoop@gmail.com>
2021-10-19 16:39:49 +0200

    get request virker

---
+++ Session07/TodosWebAPI/Controllers/TodosController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodosWebAPI.Data;
+using TodosWebAPI.Models;
+
+namespace TodosWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TodosController : ControllerBase
+    {
+        private ITodoData TodoData;
+
+        public TodosController(ITodoData todoData)
+        {
+            TodoData = todoData;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IList<Todo>>> GetTodos([FromQuery] bool? IsCompleted, [FromQuery] int? UserId)
+        {
+            try
+            {
+                IList<Todo> todos = TodoData.GetTodos();
+                if (IsCompleted!=null)
+                {
+                    todos = todos.Where(todo => todo.IsCompleted == IsCompleted).ToList();
+                }
+
+                if (UserId != null)
+                {
+                    todos = todos.Where(todo => todo.UserID == UserId).ToList();
+                }
+
+                return Ok(todos);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, e.Message);
+            }
+           
+            
+        }
+    }
+}
---
+++ Session07/TodosWebAPI/Data/ITodoData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TodosWebAPI.Models;
+
+namespace TodosWebAPI.Data
+{
+    public interface ITodoData
+    {
+        IList<Todo> GetTodos();
+        void AddTodo(Todo todo);
+        void RemoveTodo(int todoId);
+        void Update(Todo todo);
+        Todo Get(int id);
+    }
+}
