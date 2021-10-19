65c38fdcbbb42b9639d2ef7325747011e6a85fcc
Kasper <kasperscoop@gmail.com>
2021-10-19 13:41:00 +0200

    new folder

--- Session05/S5Ex01/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ Session05/S5Ex01/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace S5Ex01.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\_Imports.razor"
+#line 1 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\_Imports.razor"
+#line 2 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\_Imports.razor"
+#line 3 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\_Imports.razor"
+#line 4 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\_Imports.razor"
+#line 5 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\_Imports.razor"
+#line 6 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\_Imports.razor"
+#line 7 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\_Imports.razor"
+#line 8 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\_Imports.razor"
+#line 9 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\_Imports.razor"
 using S5Ex01;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\_Imports.razor"
+#line 10 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\_Imports.razor"
 using S5Ex01.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using S5Ex01.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 22 "D:\Dokumenter D\Git\DNP1-Assignment11\Session05\S5Ex01\Shared\MainLayout.razor"
+#line 22 "C:\Users\Kasper\Git\DNP1_Assignments\Session05\S5Ex01\Shared\MainLayout.razor"
  
     [CascadingParameter] protected Task<AuthenticationState> AuthStat { get; set; }
 
